8fcd42e4fdb9d48463e78855a01e27bde44cf9e1
Mateusz Siek <contact@msiek.com>
2023-03-29 23:49:38 +0200

    fmt fix

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,42 +1,43 @@
-import { defineManifest } from '@crxjs/vite-plugin'
+import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
-  name: 'chat-gpt-wizard',
-  description: 'A chrome extension that gives you additional control over chatGPT',
-  version: '0.0.0',
+  name: "chat-gpt-wizard",
+  description:
+    "A chrome extension that gives you additional control over chatGPT",
+  version: "0.0.0",
   manifest_version: 3,
   icons: {
-    '16': 'img/logo-16.png',
-    '32': 'img/logo-34.png',
-    '48': 'img/logo-48.png',
-    '128': 'img/logo-128.png',
+    "16": "img/logo-16.png",
+    "32": "img/logo-34.png",
+    "48": "img/logo-48.png",
+    "128": "img/logo-128.png",
   },
   action: {
-    default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_popup: "popup.html",
+    default_icon: "img/logo-48.png",
   },
-  options_page: 'options.html',
+  options_page: "options.html",
   background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module'
+    service_worker: "src/background/index.ts",
+    type: "module",
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/content/index.tsx'],
+      matches: ["http://*/*", "https://*/*"],
+      js: ["src/content/index.tsx"],
     },
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: [
+        "img/logo-16.png",
+        "img/logo-34.png",
+        "img/logo-48.png",
+        "img/logo-128.png",
+      ],
       matches: [],
     },
   ],
-  permissions: [
-    "storage",
-    "tabs"
-  ],
-  host_permissions: [
-    "<all_urls>"
-  ],
-})
+  permissions: ["storage", "tabs"],
+  host_permissions: ["<all_urls>"],
+});
