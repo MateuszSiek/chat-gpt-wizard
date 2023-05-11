396acedda3cb1eeaf5207c684efbaf2cb6e1b903
Mateusz Siek <contact@msiek.com>
2023-05-11 22:08:15 +0200

    updated extension icon

--- build/assets/content-script-loader.chunk-86f8d7a2.d4969d6d.js
+++ build/assets/content-script-loader.chunk-86f8d7a2.d4969d6d.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-03f8e72d.js")
+      chrome.runtime.getURL("assets/chunk-86f8d7a2.js")
     );
   })().catch(console.error);
 
